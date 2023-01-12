99e10047136c38ae50902e16213c053df6c7bd07
Joseph Clack <joe@joeclack.me.uk>
2023-01-12 17:58:41 +0000

    bump version

--- hoyolab_daily_bot/config.py
+++ hoyolab_daily_bot/config.py
@@ -7,7 +7,7 @@ from typing import Any, Self
 class _Config:
 
     class Meta:
-        VER = "1.2"
+        VER = "1.3"
         UPDATE_CHANNEL = "https://github.com/Lordfirespeed/hoyolab-daily-bot/releases/latest"
 
     __instance: Self = None
