01016d45caf69c8b0ecd24f5b84bf5dac62ed31f
Joseph Clack <joe@joeclack.me.uk>
2023-01-14 17:05:08 +0000

    rewrite claim by subclassing request.Session

--- hoyolab_daily_bot/claim.py
+++ hoyolab_daily_bot/claim.py
@@ -1,176 +1,137 @@
 import time
-import sys
 import requests
 from pathlib import Path
-from urllib import parse as urlparsing
-import browser_cookie3
+from urllib.parse import urlparse, ParseResult
 import logging
 from .config import Config
+from typing import TypeAlias
+
+JSON: TypeAlias = dict[str, "JSON"] | list["JSON"] | str | int | float | bool | None
 
 
-def cookies_not_found():
-    logging.info("Login information not found! Please login first to hoyolab once in Chrome/Firefox/Opera/Edge/Chromium before using the bot.")
-    logging.info("You only need to login once for a year to https://www.hoyolab.com/genshin/ for this bot to work.")
-    logging.error("LOGIN ERROR: cookies not found")
-    time.sleep(5)
-    sys.exit(1)
-
-
-def get_cookies():
-    domain_name = Config["DOMAIN_NAME"]
-    selected_brower = Config["BROWSER"].lower()
-
-    try:
-        if selected_brower == "all":
-            return browser_cookie3.load(domain_name=domain_name)
-
-        if selected_brower in ("firefox", "chrome", "opera", "edge", "chromium"):
-            return getattr(browser_cookie3, selected_brower)(domain_name=domain_name)
-
-    except Exception as error:
-        logging.error("Unexpected error while getting cookies:")
-        logging.exception(error)
-
-    cookies_not_found()
-
-
-def check_for_token_cookie(cookies):
-    token_cookies = filter(lambda cookie: cookie.name == "cookie_token", cookies)
-
-    if not token_cookies:
-        cookies_not_found()
-
-
-# GET COOKIES
-cookies = get_cookies()
-check_for_token_cookie(cookies)
-
-
-# API FUNCTIONS
-def getDailyStatus():
-    headers = {
+class HoyoverseAPISession(requests.Session):
+    api_headers = {
         'Accept': 'application/json, text/plain, */*',
         'Accept-Language': 'en-US,en;q=0.5',
         'Origin': 'https://webstatic-sea.hoyoverse.com',
         'Connection': 'keep-alive',
-        'Referer': f'https://webstatic-sea.hoyoverse.com/ys/event/signin-sea/index.html?act_id={Config["ACT_ID"]}&lang=en-us',
+        'Referer': 'https://webstatic-sea.hoyoverse.com/ys/event/signin-sea/index.html?act_id={act_id}&lang=en-us',
         'Cache-Control': 'max-age=0',
     }
 
-    params = (
-        ('lang', 'en-us'),
-        ('act_id', Config['ACT_ID']),
-    )
+    api_root: ParseResult = urlparse("https://hk4e-api-os.hoyoverse.com/")
 
-    try:
-        response = requests.get(
-            'https://hk4e-api-os.hoyoverse.com/event/sol/info',
-            headers=headers,
-            params=params,
-            cookies=cookies
+    def __init__(self):
+        super(HoyoverseAPISession, self).__init__()
+        formatted_headers = self.format_headers(HoyoverseAPISession.api_headers)
+        self.headers.update(formatted_headers)
+
+    @staticmethod
+    def format_headers(headers: dict[str, str]) -> dict[str, str]:
+        formatted_headers = {}
+        for key, value in headers:
+            formatted_headers[key] = value.format(
+                act_id=Config["ACT_ID"]
+            )
+        return formatted_headers
+
+    def api_request(self, method, path, *args, **kwargs) -> JSON:
+        target = self.api_root._replace(path=path).geturl()
+        try:
+            response = self.request(method, target, *args, **kwargs)
+        except requests.exceptions.ConnectionError as error:
+            logging.error("CONNECTION ERROR: cannot get daily check-in status")
+            logging.exception(error)
+            raise error
+
+        body = response.json()
+
+        logging.debug("API response: ")
+        logging.debug(body)
+
+        if body["retcode"] != 0:
+            raise ValueError(f"Hoyoverse API gave non-zero returncode with explanation: '{body['message']}'")
+
+        return body
+
+    def api_get(self, path, *args, **kwargs) -> JSON:
+        kwargs.setdefault("allow_redirects", True)
+        return self.api_request("GET", path, *args, **kwargs)
+
+    def api_post(self, path, *args, **kwargs) -> JSON:
+        return self.api_request("POST", path, *args, **kwargs)
+
+    def get_daily_claim_status(self) -> JSON:
+        return self.api_get(
+            "event/sol/info",
+            params={
+                "lang": "en-us",
+                "act_id": Config["ACT_ID"]
+            }
         )
-    except requests.exceptions.ConnectionError as error:
-        logging.error("CONNECTION ERROR: cannot get daily check-in status")
-        logging.exception(error)
-        raise error
 
-    logging.debug(response)
-    body = response.json()
-
-    if body["retcode"] != 0:
-        raise ValueError(f"Hoyoverse API gave non-zero returncode with explanation: '{body['message']}'")
-
-    return body
-
-
-def isClaimed():
-    resp = getDailyStatus()
-    return resp['data']['is_sign']
-
-
-def claimReward():
-    headers = {
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Content-Type': 'application/json;charset=utf-8',
-        'Origin': 'https://webstatic-sea.hoyoverse.com',
-        'Connection': 'keep-alive',
-        'Referer': f'https://webstatic-sea.hoyoverse.com/ys/event/signin- sea/index.html?act_id={Config["ACT_ID"]}&lang=en-us',
-    }
-
-    params = (
-        ('lang', 'en-us'),
-    )
-
-    data = {'act_id': Config['ACT_ID']}
-
-    try:
-        response = requests.post(
-            'https://hk4e-api-os.hoyoverse.com/event/sol/sign',
-            headers=headers,
-            params=params,
-            cookies=cookies,
-            json=data
+    def post_daily_claim(self):
+        return self.api_post(
+            "event/sol/sign",
+            params={
+                "lang": "en-us"
+            },
+            json={
+                "act_id": Config["ACT_ID"]
+            }
         )
-    except requests.exceptions.ConnectionError as error:
-        logging.error("CONNECTION ERROR: cannot claim daily check-in reward")
-        logging.exception(error)
-        raise error
-
-    logging.debug(response)
-    body = response.json()
-
-    if body["retcode"] != 0:
-        raise ValueError(f"Hoyoverse API gave non-zero returncode with explanation: '{body['message']}'")
-
-    return body
 
 
-# UPDATE CHECKER
-def checkUpdates():
-    res = requests.get(Config.Meta.UPDATE_CHANNEL)
-    newest_version = Path(urlparsing.urlparse(res.url).path).name
+class DailyClaimBot:
+    max_retries = 10
 
-    if newest_version == "releases":
-        return
+    def __init__(self) -> None:
+        self.session = HoyoverseAPISession()
 
-    newest_version = newest_version[1:]
+    def already_claimed_today(self):
+        resp = self.session.get_daily_claim_status()
+        return resp['data']['is_sign']
 
-    if newest_version > Config.Meta.VER:
-        logging.info(f'New version ({newest_version}) available!\nPlease go to {Config.Meta.UPDATE_CHANNEL} to download the new version.')
-        time.sleep(60)
+    @staticmethod
+    def check_for_package_updates():
+        res = requests.get(Config.Meta.UPDATE_CHANNEL)
+        newest_version = Path(urlparse(res.url).path).name
 
+        if newest_version == "releases":
+            return
 
-# MAIN PROGRAM
-def main():
-    logging.info("Starting claim loop...")
+        newest_version = newest_version[1:]
 
-    def attempt_claim():
+        if newest_version > Config.Meta.VER:
+            logging.info(f'New version ({newest_version}) available!\nPlease go to {Config.Meta.UPDATE_CHANNEL} to download the new version.')
+
+    def attempt_claim(self):
         logging.info("Connecting to Hoyoverse...")
-        if isClaimed():
+        if self.already_claimed_today():
             logging.info(f'Reward already claimed.')
             return
 
         logging.info("Reward not claimed yet. Claiming reward...")
-        resp = claimReward()
+        resp = self.session.post_daily_claim()
         logging.info("Reward claimed successfully.")
         logging.info(resp['message'])
 
-    while True:
-        try:
-            attempt_claim()
-        except requests.exceptions.ConnectionError:
-            logging.info("Failed to claim, waiting before retry...")
-            time.sleep(60)
-            logging.info("Retrying claim...")
-        except Exception as error:
-            logging.error("Unknown error occurred, breaking loop:")
-            logging.exception(error)
-            break
+    def main(self):
+        logging.info("Starting claim loop...")
+        for _ in range(self.max_retries):
+            try:
+                self.attempt_claim()
+                break
+            except requests.exceptions.ConnectionError:
+                logging.info("Failed to claim, waiting before retry...")
+                time.sleep(60)
+                logging.info("Retrying claim...")
+            except Exception as error:
+                logging.error("Unexpected error occurred, breaking loop:")
+                logging.exception(error)
+                break
 
-        break
-
-    checkUpdates()
+        self.check_for_package_updates()
 
 
 if __name__ == "__main__":
@@ -184,5 +145,6 @@ if __name__ == "__main__":
         ]
     )
 
-    main()
+    bot = DailyClaimBot()
+    bot.main()
     time.sleep(2)
