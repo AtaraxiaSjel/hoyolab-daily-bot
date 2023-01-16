140ccfbd09bd0a401f9f406bcf6c662e96a6119b
Lordfirespeed <joe@joeclack.me.uk>
2023-01-16 01:28:40 +0000

    bump version

--- hoyolab_daily_bot/config.py
+++ hoyolab_daily_bot/config.py
@@ -7,7 +7,7 @@ from typing import Any, Self
 class _Config:
 
     class Meta:
-        VER = "1.4.2"
+        VER = "1.5.0"
         UPDATE_CHANNEL = "https://github.com/Lordfirespeed/hoyolab-daily-bot/releases/latest"
 
     __instance: Self = None
