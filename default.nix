184e85c286e7049014f0ce47016afb49426b2f16
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2023-05-02 22:42:14 +0300

    add initial flake for NixOS

---
+++ default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  python3Packages,
+  version ? "git",
+}:
+with python3Packages; buildPythonApplication rec {
+  pname = "hoyolab-daily-bot";
+  inherit version;
+
+  src = lib.cleanSource ./.;
+
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  propagatedBuildInputs = [
+    requests
+    browser-cookie3
+    tzdata
+    python-crontab
+    typing-extensions
+    # dbus-python #FIXME: browser-cookie3 expect kdewallet (dbus)
+  ];
+
+  meta = with lib; {
+    homepage = "https://github.com/AtaraxiaSjel/hoyolab-daily-bot";
+    description = "Hoyolab Daily Check-in Bot.";
+    license = licenses.mit;
+    platforms = platforms.linux;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Hoyolab Daily Check-in Bot.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    inherit (nixpkgs) lib;
+    genSystems = lib.genAttrs [
+      "x86_64-linux"
+    ];
+    pkgsFor = nixpkgs.legacyPackages;
+  in {
+    overlays.default = _: prev: rec {
+      hoyolab-daily-bot = prev.callPackage ./default.nix {
+        version = "1.0";
+      };
+    };
+    packages = genSystems (system:
+      (self.overlays.default null pkgsFor.${system})
+      // {
+        default = self.packages.${system}.hoyolab-daily-bot;
+      });
+  };
+
+  nixConfig = {
+    extra-substituters = ["https://ataraxiadev-foss.cachix.org"];
+    extra-trusted-public-keys = ["ataraxiadev-foss.cachix.org-1:ws/jmPRUF5R8TkirnV1b525lP9F/uTBsz2KraV61058="];
+  };
+}
